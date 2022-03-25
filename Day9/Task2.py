4f4910e350d8e89c92b3a98ae67e109d2d295345
ap-101 <83851593+ap-101@users.noreply.github.com>
2022-03-25 22:12:44 +0530

    Create Task2.py

---
+++ Day9/Task2.py
@@ -0,0 +1,11 @@
+
+'''for i in range(20):
+    print('❤', end="",)'''
+
+b = int(input('\n Enter The Number of Rows '))
+for a in range( b):
+    for c in range(0, a + 1):
+        print('❤\n')
+
+
+print()
